a9f776778eeb624d7e0d3fb7717f7b6c93f7bbbf
Franck Delache <franck.delache@gmail.com>
2024-11-18 21:17:55 -0500

    Allow reusing slices from one map to another

--- lib/parallel_enumerable.rb
+++ lib/parallel_enumerable.rb
@@ -4,24 +4,29 @@ require 'etc'
 
 module Enumerable
   def parallel
-    ParallelEnumerable.new(self)
+    ParallelEnumerable.from_enumerable(self)
   end
 end
 
 class ParallelEnumerable
   include Enumerable
 
-  # @param original_collection: The Enumerable to parallelize.
-  def initialize(original_collection)
-    @original_collection = original_collection
-  end
+  class << self
+    def from_enumerable(enumerable)
+      new(enumerable:)
+    end
 
+    def from_slices(slices)
+      new(slices:)
+    end
+  end
+  
   def unparallel
-    @original_collection
+    enumerable
   end
 
   def each(&block)
-    @original_collection.each(&block)
+    enumerable.each(&block)
   end
 
   def map(&block)
@@ -33,23 +38,35 @@ class ParallelEnumerable
       end
     end
 
-    ParallelEnumerable.new(ractors.flat_map(&:take))
+    ParallelEnumerable.from_slices(ractors.map(&:take))
   end
 
   private
 
+  def initialize(enumerable: nil, slices: nil)
+    # We only accept one or the other, but not both.
+    raise unless enumerable.nil? ^ slices.nil?
+
+    @enumerable = enumerable
+    @slices = slices
+  end
+
   def ractor_count
-    [processor_count, @original_collection.count].min
+    [processor_count, enumerable.count].min
   end
 
   def processor_count
     Etc.nprocessors
   end
 
+  def enumerable
+    @enumerable ||= slices.flatten
+  end
+
   def slices
-    unless defined?(@slices)
-      slice_size = @original_collection.count / ractor_count
-      @slices = @original_collection.each_slice(slice_size)
+    if @slices.nil?
+      slice_size = enumerable.count / ractor_count
+      @slices = enumerable.each_slice(slice_size)
     end
 
     @slices
--- test/unit/parallel_enumerable_test.rb
+++ test/unit/parallel_enumerable_test.rb
@@ -6,19 +6,19 @@ class ParallelEnumerableTest < Minitest::Test
     original_collection = 1.step.take(10)
 
     expected_result = original_collection.map { |n| n + 1 }
-    assert_equal expected_result.to_a, ParallelEnumerable.new(original_collection).map { |n| n + 1 }.to_a
+    assert_equal expected_result.to_a, original_collection.parallel.map { |n| n + 1 }.to_a
   end
 
   def test_reduce
     original_collection = 1.step.take(10)
 
     expected_result = original_collection.reduce(&:+)
-    assert_equal expected_result, ParallelEnumerable.new(original_collection).reduce(&:+)
+    assert_equal expected_result, original_collection.parallel.reduce(&:+) 
   end
 
   def test_unparallel
     original_collection = 1.step.take(10)
 
-    assert_equal original_collection, ParallelEnumerable.new(original_collection).unparallel
+    assert_equal original_collection, original_collection.parallel.unparallel 
   end
 end
